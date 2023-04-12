e6b831211cfc90fcafad0d9fd724cbfc28da4c17
NGUYỄN TRƯỜNG GIANG <91778158+nguyentruonggiang281@users.noreply.github.com>
2023-04-13 00:59:36 +0700

    Delete SpringJsp directory

--- SpringJsp/src/main/java/com/example/springjsp/repository/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springjsp.repository;
-
-import com.example.springjsp.model.Booking;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookingRepository extends CrudRepository<Booking, Long> {
-}
--- SpringJsp/src/main/java/com/example/springjsp/repository/TourRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springjsp.repository;
-
-import com.example.springjsp.model.Tour;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TourRepository extends JpaRepository<Tour, Long> {
-}
